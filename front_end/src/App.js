6143499f3b6b8f0c764ad9acd35026775e93d18f
guenaguensoumaya <guenaguensoumaya13@gmail.com>
2023-03-05 19:07:40 +0100

    first commit

---
+++ front_end/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Home from './Pages/Home';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
+function App() {
+  return (
+    <div>
+       <p>viroa</p>
+    </div>
+  );
+}
+export default App;
